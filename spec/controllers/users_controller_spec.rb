01b5c3b0a05d2dc3be993c1830e554502b823697
Gonzalo Gárate <ggarateramirez@gmail.com>
2011-05-31 23:05:53 +0200

    Hecho Inicio Sesion

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -45,7 +45,19 @@ describe UsersController do
       get 'new'
       response.should have_selector("title", :content => "Sign up")
     end
+
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'][type='text']")
+    end
+
+    it "should have an email field"
+
+    it "should have a password field"
+
+    it "should have a password confirmation field"
   end
+
 
   describe "POST 'create'" do
 
@@ -94,6 +106,11 @@ describe UsersController do
       it "should have a welcome message" do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to the sample app/i
+      end
+
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
       end
     end
   end
